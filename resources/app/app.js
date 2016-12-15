9200be3da6109586dfe231fdf0f09f35852cd87d
cdbullones <carlosdavidbullones@gmail.com>
2016-12-15 09:41:42 -0400

    Update app.js
    Fix: Se realizaron ajustes en el app.js para limpiar el select, inicializando la variable en el modelo angular.

--- resources/app/app.js
+++ resources/app/app.js
@@ -4,12 +4,16 @@ angular
 
 function controladorPrincipal($http) {
     var vm = this;
+    vm.url = 'Seleccione';
 
-
-    vm.buscaEnRegion = function () {
-        $http.get(vm.url).success(function (response) {
-            //console.log("res:", respuesta);
-            vm.fuerzas = response.results;
-        });
+    vm.buscarData = function () {
+        if (vm.url == 'Seleccione') {
+            vm.fuerzas = '';
+        } else {
+            $http.get(vm.url).success(function (response) {
+                //console.log("res:", respuesta);
+                vm.fuerzas = response.results;
+            });
+        };
     }
 }
